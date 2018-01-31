9e8580c729020bcec118aed8ea2890db40e7a8c8
Munkei <stareye863@gmail.com>
2018-01-31 14:11:53 +0100

    Added Methods to the bike classes, and increment the ID using a static int

--- Bike_Rental/Model/EBike.cs
+++ Bike_Rental/Model/EBike.cs
@@ -1,12 +1,169 @@
-﻿using System;
+﻿///Author:Dominique Amir Köstler
+///Klasse:IA116
+///
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Bike_Rental.Model
-{
+namespace Bike_Rental
+{ 
     class EBike
     {
+        #region Members
+        private int _id;
+        private int _size;
+        private string _kind;
+        private int _usedTime;
+        private bool _status;
+        private string _location;
+        private double _cost;
+        private double _powerLevel;
+        private static int _bikeCounter;
+        #endregion
+        #region Properties
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+
+            set
+            {
+                _id = value;
+            }
+        }
+
+        public int Size
+        {
+            get
+            {
+                return _size;
+            }
+
+            set
+            {
+                _size = value;
+            }
+        }
+
+        public string Kind
+        {
+            get
+            {
+                return _kind;
+            }
+
+            set
+            {
+                _kind = value;
+            }
+        }
+
+        public int UsedTime
+        {
+            get
+            {
+                return _usedTime;
+            }
+
+            set
+            {
+                _usedTime = value;
+            }
+        }
+
+        public bool Status
+        {
+            get
+            {
+                return _status;
+            }
+
+            set
+            {
+                _status = value;
+            }
+        }
+
+        public string Location
+        {
+            get
+            {
+                return _location;
+            }
+
+            set
+            {
+                _location = value;
+            }
+        }
+
+        public double Cost
+        {
+            get
+            {
+                return _cost;
+            }
+
+            set
+            {
+                _cost = value;
+            }
+        }
+
+        public double PowerLevel
+        {
+            get
+            {
+                return _powerLevel;
+            }
+
+            set
+            {
+                _powerLevel = value;
+            }
+        }
+
+        public static int BikeCounter
+        {
+            get
+            {
+                return _bikeCounter;
+            }
+
+            set
+            {
+                _bikeCounter = value;
+            }
+        }
+
+        #endregion
+        #region Constructor
+        public EBike(string kindValue,string locationValue)
+        {
+            this.IncrementBikeCount();
+            this.Id = EBike.BikeCounter;
+            this.Size = 30;
+            this.Kind = kindValue;
+            this.UsedTime = 40;
+            this.Status = true;
+            this.Location = locationValue;
+            this.Cost = 7.5;
+            this.PowerLevel = 9001; //OVER NINE THOUSAND!
+        }
+        #endregion
+        #region Methods
+        public void StartCycling()
+        {
+            Console.WriteLine("Jetzt gehts losssssssss!!");
+        }
+        public void IncrementBikeCount()
+        {
+            EBike.BikeCounter += 1;
+        }
+        #endregion
     }
 }
